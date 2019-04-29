be7c07bba17bab3413e9db6a640bbb83c5c97c9e
WJXHenry <wjxhenry@gmail.com>
2019-04-29 12:17:02 -0600

    Water levels controller, returns average and delta over different time periods

--- src/controllers/water-level-changes.js
+++ src/controllers/water-level-changes.js
@@ -2,22 +2,24 @@ const request = require('request-promise-native')
 const uuid = require('uuid/v4')
 
 const TIMEZONE_OFFSET_MILLIS = 360 * 60 * 1000 // Timezone offset for Edmonton to UTC
+const key = 'water-level-changes' // Unique key for dataset storage
+const url = 'https://data.edmonton.ca/resource/cnsu-iagr.json' // The Socrata api endpoint
+const queryBase = `${url}?$query=`
 
 /**
  * Water Level Changes
  */
 module.exports = async function(req, res) {
   console.log('Water Level Changes Controller')
-  let key = 'water-level-changes' // Unique key for dataset storage
-  let url = 'https://data.edmonton.ca/resource/cnsu-iagr.json' // The Socrata api endpoint
-  let queryBase = `${url}?$query=`
-
   let storedData = await req.cache.getLatest(key)
   let storedUpdated
   if (storedData) storedUpdated = storedData.last_updated
 
-  // Get all the columns (filter it by column later)
-  let getUpdatedQuery = `${queryBase}SELECT date_and_time, water_level_m WHERE station_number = '05DF001' ORDER BY date_and_time DESC`
+  let getUpdatedQuery = `${queryBase}
+    SELECT date_and_time, water_level_m
+    WHERE station_number = '05DF001'
+    ORDER BY date_and_time DESC
+    LIMIT 1`
   let updatedData
   let recentUpdated
   try {
@@ -29,30 +31,45 @@ module.exports = async function(req, res) {
     throw e
   }
 
-  console.log(recentUpdated)
-  let bounds = getBounds(recentUpdated)
-  console.log('Lower: ' + bounds[0])
-  console.log('Upper: ' + bounds[1])
-
   let responseValues
   if (storedUpdated && new Date(storedUpdated) >= new Date(recentUpdated)) {
     console.log('No new updates')
     responseValues = await req.cache.getAll(key, req.body['limit'])
   } else {
     console.log('Dataset was updated')
+    let oneDayBounds = getBounds(recentUpdated, 1)
+    let threeDaysBounds = getBounds(recentUpdated, 3)
+    let oneWeekBounds = getBounds(recentUpdated, 7)
+    let oneDayResults
+    let threeDaysResults
+    let oneWeekResults
+    try {
+      await Promise.all(
+        (oneDayResults = await getData(oneDayBounds)),
+        (threeDaysResults = await getData(threeDaysBounds)),
+        (oneWeekResults = await getData(oneWeekBounds))
+      )
+    } catch (e) {
+      e.code = 500
+      throw e
+    }
     console.log('Adding new rows')
     let id = uuid()
     let newRows = {
       id,
       created_at: new Date(Date.now()).toISOString(),
       last_updated: recentUpdated,
+      one_day_average: oneDayResults[0].toFixed(3),
+      one_day_delta: oneDayResults[1].toFixed(3),
+      three_day_average: threeDaysResults[0].toFixed(3),
+      three_day_delta: threeDaysResults[1].toFixed(3),
+      one_week_average: oneWeekResults[0].toFixed(3),
+      one_week_delta: oneWeekResults[1].toFixed(3),
       meta: {
         id,
         timestamp: Math.round(new Date() / 1000)
       }
     }
-    // TODO: add ingredients like average water level for the day?
-    // Use the SoQL to do this -> avg, etc... -> is probably faster
     req.cache.add(key, newRows)
     responseValues = await req.cache.getAll(key, req.body['limit'])
   }
@@ -63,17 +80,50 @@ module.exports = async function(req, res) {
 }
 
 /**
- * This function obtains the upper and lower bounds of a three-day period
+ * This function obtains the upper and lower bounds of a period
  * @param {String} date The date in ISO format
- * @returns {Array<String>} The upper and lower bounds of the three-day period
+ * @param {Number} days The number of days the period should be
+ * @returns {Array<String>} The upper and lower bounds of the period
  */
-function getBounds(date) {
+function getBounds(date, days) {
   const dateRegex = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}/
   let currentDate = new Date(new Date(date) - TIMEZONE_OFFSET_MILLIS)
   let threeDayPeriod = new Date(
-    new Date(date) - (3 * 24 * 60 * 60 * 1000 + TIMEZONE_OFFSET_MILLIS)
+    new Date(date) - (days * 24 * 60 * 60 * 1000 + TIMEZONE_OFFSET_MILLIS)
   )
-  let lowerBound = threeDayPeriod.toISOString().match(dateRegex)
-  let upperBound = currentDate.toISOString().match(dateRegex)
+  let lowerBound = threeDayPeriod.toISOString().match(dateRegex)[0]
+  let upperBound = currentDate.toISOString().match(dateRegex)[0]
   return [lowerBound, upperBound]
+}
+
+/**
+ * Returns the average and bounds respecitvely in an array
+ * @param {Array<String>} bounds The bounds to do calculation on (dates in ISO 8601 format)
+ * @returns {Array<Number>} The average and delta of the water level between the specified bounds
+ */
+async function getData(bounds) {
+  let workingDataQuery = `${queryBase}
+    SELECT water_level_m
+    WHERE station_number = '05DF001'
+    AND date_and_time
+    BETWEEN '${bounds[0]}'
+    AND '${bounds[1]}'
+    ORDER BY date_and_time DESC
+    LIMIT 10000`
+  let workingData
+  try {
+    let rawJsonData = await request(workingDataQuery)
+    workingData = JSON.parse(rawJsonData).map(data => {
+      return Number(data.water_level_m)
+    })
+  } catch (e) {
+    e.code = 500
+    throw e
+  }
+  let delta = workingData[0] - workingData[workingData.length - 1]
+  let sum = workingData.reduce(function(a, b) {
+    return a + b
+  })
+  let average = sum / workingData.length
+  return [average, delta]
 }
